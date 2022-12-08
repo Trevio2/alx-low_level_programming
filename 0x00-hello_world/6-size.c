ce04c884b913d73198a19a77ef6765f5c9275767
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-08 08:55:24 -0800

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - main block
+ */
+int main(void)
+{
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of char: %zu byte\n", sizeof(char));
+return (0);
+}
