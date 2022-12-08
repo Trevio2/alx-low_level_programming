8b38a77e80de86b53786120fcae1534ffaddb2e9
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-08 07:40:23 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - main block
  *return: o
  */
+
 int main(void)
-{
+{  
       puts("\"Programming is like building a multilingual puzzle");
-      return (0)
+      return (0);
+      
 }
