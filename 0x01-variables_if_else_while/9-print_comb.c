bf0f60bad475fff6ce00ecbd7e85867fcf6946d4
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-09 16:01:49 -0800

    print9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+int number;
+for (number = 0; number <= 9; number++)
+{
+putchar(number + '0');
+if (number < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
