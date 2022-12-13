b02d19cfc451d00bd880b4ed558e0abe2310e3d1
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-13 15:05:01 -0800

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	unsigned long int i,j,k,j1,j2,k1,k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
-	printf("%lu",j);
-	for(i = 1; i < 91; i++)
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
 	printf(", %lu", k);
 	k = k + j;
