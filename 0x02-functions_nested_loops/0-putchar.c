54b71c0e7a25d17f2d236bbeae26a5fefef10293
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-13 04:07:33 -0800

    putchar0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
