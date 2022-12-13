79677995e7cd6f8c9c4e6b2274baa3314fc7e7c5
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-13 14:56:45 -0800

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,56 +1,37 @@
 #include <stdio.h>
 /**
- * numlength - returns the length of string
- * @num: operator number
- * Return: number of digit
- */
-int numlength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-	return (1);
-	}
-	while (num)
-	{
-	num = num / 10;
-	length += 1;
-	}
-	return (length);
-}
-/**
  * main - print the first 98 fibonaci sequences
+ * starting with 1 and 2
  * Return: 0
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
-	short int i = 1, initialos;
+	unsigned long int i,j,k,j1,j2,k1,k2;
 
-	while (i <= 98)
+	j = 1;
+	k = 2;
+	printf("%lu",j);
+	for(i = 1; i < 91; i++)
 	{
-	if (f10 = 0)
-	printf("%lu", f10);
-	initialos = numlength(mx) - 1 - numlength(f1);
-	while (f10 >  0 && initialos > 0)
-	{
-	printf("%lu", 0);
-	initialos--;
+	printf(", %lu", k);
+	k = k + j;
+	j = k - j;
 	}
-	printf("%lu", f1);
 
-	tmp = (f1 + f2) % mx;
-	tmpo = f10 + f20 = (f1 + f2) / mx;
-	f1 = f2;
-	f10 = f20;
-	f2 = tmp;
-	f20 = tmpo;
-	if (i != 98)
-	printf (", ");
-	else
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+	printf(", %lu", k1 + (k2 / 1000000000));
+	printf("%lu", k2 % 1000000000);
+	k1 = k1 + j1;
+	j1 = k1 - j1;
+	k2 = k2 + j2;
+	j2 = k2 - j2;
+	}
 	printf("\n");
-	i++
-	}
 	return (0);
 }
