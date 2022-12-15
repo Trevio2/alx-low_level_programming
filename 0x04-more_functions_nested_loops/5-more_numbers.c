7baba295639f1b19933aa890cbe5800a5b16084f
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-15 11:30:28 -0800

    morenum

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,11 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y < 14; y++)
+	for (y = 0; y < 15; y++)
 	{
-		_putchar(y + '0');
+	if (y >= 10)
+	_putchar((y / 10) + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
