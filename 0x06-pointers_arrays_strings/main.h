60021e54cd57e334acdc5cb52539c2e04547ff5e
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-21 13:08:25 -0800

    infinite

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 
 #endif
