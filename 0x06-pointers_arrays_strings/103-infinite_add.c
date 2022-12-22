9d3797ed3bf8fea08dc7a71fb8c86d79bebee082
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-22 00:22:20 -0800

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -44,3 +44,27 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (0);
 	return (r + r_index + 1);
 }
+/**
+ * infinite_add - adds two number
+ * @n1: the first number
+ * @n2: the second
+ * @r: the buffer
+ * @size_r: the buffer size
+ * Return: if r can store the sum
+ * if r cannot store the sum - 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int index, n1_len = 0, n2_len = 0;
+
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+	return (add_strings(n1, n2, r, --size_r));
+}
