c45ff0c8c789f3f625121d5cbf22e55aaa4f1534
Trevio2 <gilbertonyinyechi1@gmail.com>
2022-12-22 16:13:09 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,24 +2,26 @@
 
 /**
  * leet - function that encode a string to 1337
- * @str: the string
+ * @x: the string
  * Return: the string encoded
  */
 
-char *leet(char *str)
+char *leet(char *x)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', '0', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (str[index1])
+	while (x[a])
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		b = 0;
+		while (b < l)
 		{
-			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2])
-			str[index1] = index2 + '0';
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] = trw[b];
+			b++;
 		}
-		index1++;
+		a++;
 	}
-	return (str);
+	return (x);
 }
