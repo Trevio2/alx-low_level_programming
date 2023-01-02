df8f7e6bc357c069534b1971ca70d2f2c1b5594b
Trevio2 <gilbertonyinyechi1@gmail.com>
2023-01-02 08:51:38 -0800

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
+}
+
