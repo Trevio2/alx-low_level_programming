b784c0d30563d51fd1ee501acaa59165e9f0fa0e
Trevio2 <gilbertonyinyechi1@gmail.com>
2023-01-06 11:15:33 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
