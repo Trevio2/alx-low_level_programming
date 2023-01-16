91d4503541b191abd455035ea796e19689d17668
Trevio2 <gilbertonyinyechi1@gmail.com>
2023-01-16 12:58:01 -0800

    process

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,3 @@
+#ifndef PI
+#define Pi 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file 
+ * Return: 0
+ */
+int main(void)
+{
+  printf("%s\n", _file_);
+  return (0);
+}
